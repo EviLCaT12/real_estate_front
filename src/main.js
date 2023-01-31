09601e536ce3c620270229453132d8f82a58946e
EviLCaT12 <kuznetsov.timofey.2003@mail.ru>
2023-01-31 20:57:16 +1000

    add router.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import router from "@/router/router";
 
 createApp(App)
-    .use(BootstrapVue3)
+    .use(BootstrapVue3, router)
     .mount('#app')
