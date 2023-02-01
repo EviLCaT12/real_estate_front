ee7ceb88462bdb64bee81ae901167f1b59fb8cd1
Andrey-Rudenko <62091872+Andrey-Rudenko@users.noreply.github.com>
2023-02-02 06:29:54 +1000

    Rieltors page layout

---
+++ src/components/UI/RieltorCardNone.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="rielCard">
+    <h3 style="margin: 30px; margin-left: auto; margin-right: auto"><strong>У вас ещё нет ни одного риелтора</strong></h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RieltorCardNone"
+}
+</script>
+
+<style scoped>
+.rielCard {
+  margin-left: auto;
+  margin-right: auto;
+  background: #FFFFFF;
+  border: 1px solid #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 22px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+#person {
+  margin: 5px;
+}
+#outStdBtn {
+  margin: 20px;
+  width: 150px;
+}
+
+</style>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import LoginPage from "@/components/Pages/LoginPage";
 import RegPage from "@/components/Pages/RegPage";
 import MainPage from "@/components/Pages/MainPage";
 import ClientPage from "@/components/Pages/ClientPage";
+import RieltorsPage from "@/components/Pages/RieltorsPage";
 
 export default createRouter({
     history:createWebHashHistory(),
@@ -11,5 +12,6 @@ export default createRouter({
         {path: '/login', component:LoginPage},
         {path: '/reg', component: RegPage},
         {path: '/clients', component: ClientPage},
+        {path: '/rieltors', component: RieltorsPage},
     ]
 })
