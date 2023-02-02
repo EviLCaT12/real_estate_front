44f6ff0139229609c35734d35e4f8a9985c1fc41
Andrey-Rudenko <62091872+Andrey-Rudenko@users.noreply.github.com>
2023-02-02 14:16:53 +1000

    Page objects layout

---
+++ src/components/Pages/ObjectsPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <NavClient/>
+  <div class="forms">
+    <ObjectsFormAdd/>
+    <ObjectsFormChange/>
+  </div>
+  <ObjectsTable/>
+
+</template>
+
+<script>
+import ObjectsTable from "@/components/UI/ObjectsTable.vue";
+import ObjectsFormAdd from "@/components/UI/ObjectsFormAdd.vue";
+import ObjectsFormChange from "@/components/UI/ObjectsFormChange.vue";
+import NavClient from "@/components/UI/NavClient.vue";
+
+export default {
+  name: "ObjectsPage",
+  components: {NavClient, ObjectsFormChange, ObjectsFormAdd, ObjectsTable}
+}
+</script>
+
+<style scoped>
+.forms {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,17 +1,19 @@
 import {createRouter, createWebHashHistory} from "vue-router";
 import LoginPage from "@/components/Pages/LoginPage";
 import RegPage from "@/components/Pages/RegPage";
-import MainPage from "@/components/Pages/MainPage";
+// import MainPage from "@/components/Pages/MainPage";
 import ClientPage from "@/components/Pages/ClientPage";
 import RieltorsPage from "@/components/Pages/RieltorsPage";
+import ObjectsPage from "@/components/Pages/ObjectsPage.vue";
 
 export default createRouter({
     history:createWebHashHistory(),
     routes: [
-        {path: '/', component: MainPage, name: 'MainPage'},
+        // {path: '/', component: MainPage, name: 'MainPage'},
         {path: '/login', component:LoginPage},
         {path: '/reg', component: RegPage},
         {path: '/clients', component: ClientPage},
         {path: '/rieltors', component: RieltorsPage},
+        {path: '/objects', component: ObjectsPage}
     ]
 })
