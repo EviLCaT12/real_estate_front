13f4c8f42c067b23482ec134fc8d8d64af2368ff
Andrey-Rudenko <62091872+Andrey-Rudenko@users.noreply.github.com>
2023-02-03 15:31:10 +1000

    Правки вёрстки

--- src/components/ClientsTable.vue
+++ src/components/ClientsTable.vue
@@ -1,86 +1,70 @@
 <template>
-  <table class="table table-bordered">
-    <thead id="tableHead">
-    <tr>
-      <th colspan="4">Список ваших текущих клиентов</th>
-    </tr>
-    </thead>
-    <thead>
-    <tr>
-      <th scope="col">ФИО</th>
-      <th scope="col">Телефон</th>
-      <th scope="col">Почта</th>
-      <th scope="col"></th>
-    </tr>
-    </thead>
+  <div class="table-responsive">
+    <table class="table table-bordered">
+      <thead id="tableHead">
+      <tr>
+        <th colspan="4">Список ваших текущих клиентов</th>
+      </tr>
+      </thead>
+      <thead>
+      <tr>
+        <th scope="col">ФИО</th>
+        <th scope="col">Телефон</th>
+        <th scope="col">Почта</th>
+        <th scope="col"></th>
+      </tr>
+      </thead>
 
-    <tbody>
-    <tr>
-      <td>Сюда Имя</td>
-      <td>Сюда телефон</td>
-      <td>Сюда почту</td>
-      <td>
-        <button type="button" class="btn btn-primary" id="stdButton" >Профиль</button>
-        <button type="button" class="btn btn-danger" id="dngButton">Завершить</button>
-      </td>
-    </tr>
-    </tbody>
+      <tbody>
+      <tr>
+        <td>Иванов Иван Иванович</td>
+        <td>+7-999-999-99-99</td>
+        <td>pochtaclienta322@mail.ru</td>
+        <td>
+          <button type="button" class="btn btn-primary" id="stdButton">Профиль</button>
+          <button type="button" class="btn btn-danger" id="dngButton">Завершить</button>
+        </td>
+      </tr>
+      </tbody>
 
-
-
-
-
-
-    <tbody>
-    <tr>
-      <td>Сюда Имя</td>
-      <td>Сюда телефон</td>
-      <td>Сюда почту</td>
-      <td>
-        <button type="button" class="btn btn-primary" id="stdButton" >Профиль</button>
-        <button type="button" class="btn btn-danger" id="dngButton">Завершить</button>
-      </td>
-    </tr>
-    </tbody>
-    <tbody>
-    <tr>
-      <td>Сюда Имя</td>
-      <td>Сюда телефон</td>
-      <td>Сюда почту</td>
-      <td>
-        <button type="button" class="btn btn-primary" id="stdButton" >Профиль</button>
-        <button type="button" class="btn btn-danger" id="dngButton">Завершить</button>
-      </td>
-    </tr>
-    </tbody>
-    <tbody>
-    <tr>
-      <td>Сюда Имя</td>
-      <td>Сюда телефон</td>
-      <td>Сюда почту</td>
-      <td>
-        <button type="button" class="btn btn-primary" id="stdButton" >Профиль</button>
-        <button type="button" class="btn btn-danger" id="dngButton">Завершить</button>
-      </td>
-    </tr>
-    </tbody>
-    <tbody>
-    <tr>
-      <td>Сюда Имя</td>
-      <td>Сюда телефон</td>
-      <td>Сюда почту</td>
-      <td>
-        <button type="button" class="btn btn-primary" id="stdButton" >Профиль</button>
-        <button type="button" class="btn btn-danger" id="dngButton">Завершить</button>
-      </td>
-    </tr>
-    </tbody>
+      <tbody>
+      <tr>
+        <td>Иванов Иван Иванович</td>
+        <td>+7-999-999-99-99</td>
+        <td>pochtaclienta322@mail.ru</td>
+        <td>
+          <button type="button" class="btn btn-primary" id="stdButton">Профиль</button>
+          <button type="button" class="btn btn-danger" id="dngButton">Завершить</button>
+        </td>
+      </tr>
+      </tbody>
+      <tbody>
+      <tr>
+        <td>Иванов Иван Иванович</td>
+        <td>+7-999-999-99-99</td>
+        <td>pochtaclienta322@mail.ru</td>
+        <td>
+          <button type="button" class="btn btn-primary" id="stdButton">Профиль</button>
+          <button type="button" class="btn btn-danger" id="dngButton">Завершить</button>
+        </td>
+      </tr>
+      </tbody>
+      <tbody>
+      <tr>
+        <td>Иванов Иван Иванович</td>
+        <td>+7-999-999-99-99</td>
+        <td>pochtaclienta322@mail.ru</td>
+        <td>
+          <button type="button" class="btn btn-primary" id="stdButton">Профиль</button>
+          <button type="button" class="btn btn-danger" id="dngButton">Завершить</button>
+        </td>
+      </tr>
+      </tbody>
 
 
 
-
-
-  </table>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -93,7 +77,7 @@ export default {
 table {
   background-color: white;
   border-color: #5F77BF;
-  max-width: 1000px;
+  max-width: 850px;
   margin-left: auto;
   margin-right: auto;
   border-radius: 12px;
